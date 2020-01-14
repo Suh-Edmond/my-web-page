e67877bcf56de2effabe87c98e7503e2431c50d5
Edmond <suhedmond25@yahoo.com>
2020-01-14 09:30:08 +0100

    Homepage.php

---
+++ createAccount.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Decorders and Dishes made Easy</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class ="container">
+             <p>her</p>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Decorders and Dishes made Easy</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class ="container">
+             <header>
+                 <div class="row">
+                     <div class="col-md-6">
+                         <h1><dfn><label>Quick Decoders</label> Installation</dfn></h1>
+                     </div>
+                     <div class="col-md-6" id="account" >
+                        <h2 class="dropdown">
+
+                                <button  type="button"  class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                                    <span class="glyphicon glyphicon-user">
+                                    </span>
+                                </button>
+
+                            <div class="dropdown-menu">
+                                <a href="#" class="drop">Sign In</a>
+                                <div class="dropdown-divider"></div>
+                                <a href="#"  class="drop">Log out</a>
+                            </div>
+                        </h2>
+
+                     </div>
+                 </div>
+                 <div class="row" id="nav">
+                     <nav class="navbar navbar-expand-sm  navbar-dark navbar-inverse bg-light" id="navbar">
+                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#data_target">
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                         </button>
+                         <div class="collapse navbar-collapse" id="data_target">
+
+                             <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home"></span></a>
+                             <ul class="nav navbar-nav">
+                                 <li class="nav-item">
+                                     <a class="nav-link" href="#">Products</a>
+                                 </li> <li class="nav-item">
+                                     <a class="nav-link" href="#">Services</a>
+                                 </li> <li class="nav-item">
+                                     <a class="nav-link" href="#">Contact</a>
+                                 </li>
+                                 </li> <li class="nav-item">
+                                     <a class="nav-link" href="#">About Us</a>
+                                 </li>
+                                 <li class="nav-item dropdown">
+                                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" data-target="navbar"  href="#">
+                                         Account
+                                         <span class="caret"></span>
+                                     </a>
+                                     <div class="dropdown-menu " aria-labelledby="navbar">
+                                         <a class="nav-link" href="#">Create Account</a>
+                                     </div>
+                                 </li>
+
+                             </ul>
+                         </div>
+                     </nav>
+                 </div>
+
+             </header>
+           <div class="row" id="image_cat">
+               <div class="col-md-6">
+                   <p class="image1_name">Flat Screen TV and Decorder</p>
+                   <img src="image2.png" class="img-fluid img-thumbnail">
+
+               </div>
+               <div class="col-md-6">
+                   <p class="image2_name">High Quality Decorder with Good Resolution</p>
+                   <img src="image4.png" class="img-fluid img-thumbnail" id="image4"><br>
+
+               </div>
+           </div>
+           <div class="row">
+               <div class="message">
+                   <h2 class="head"><strong> Our Motivation</strong></h2>
+                   <p class="motivation">To Improved access to information in the world through the use of the Television
+                       Provide an Efficient and Easier way to get your best Decorders and Dishes shipped and installed to you.
+                       Provide full Maintenance of your Products after Installation.</p>
+               </div>
+           </div>
+           <div class="row">
+               <div id="footer" class="col-md-12">
+                   <div class="col-md-6">
+                        <div class="follow">
+                            <h3><strong>Fellow Us <dfd>>>>>>></dfd></strong> </h3>
+                            <table class="follow">
+                                <tr>
+                                    <td>Whatsapp:</td>
+                                    <td class="value">673660071 </td>
+                                </tr>
+                                <tr>
+                                    <td>Email:</td>
+                                    <td class="value">suhedmond25@yahoo.com</td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        Email:
+                                    </td>
+                                    <td class="value">
+                                    suhedmond11@gmail.com
+                                    </td>
+                                </tr>
+                                <br>
+                                <tr>
+                                    <td>Githup:</td>
+                                    <td class="value">githup.com/Suh-Edmond </td>
+                                </tr>
+                            </table>
+                            <div class="subscribe">
+                                <button type="submit" id="subscribe" class="btn btn-primary"name="sign in">
+                                   <strong><span class="glyphicon glyphicon-user" id="user"></span></strong> Subscribe</button>
+                            </div>
+                        </div>
+                   </div>
+                   <div class="col-md-6">
+                       <div class="share">
+                           <h3 class="h3"><strong>Share This Page</strong></h3>
+                           <table>
+                               <tr>
+                                   <td  class="pic"><a href="www.facebook.com"><img src="facebook.png" width="38px" height="38px"/></a></td>
+                                   <td  class="pic"><a href="www.yahoo.com"><img src="yahoo.png" width="38px" height="38px"/></a></td>
+                                   <td class="pic"><a href="www.instagram.com"><img src="instagram.png"width="38px" height="38px" /></a></td>
+                                   <td  class="pic"><a href="www.pinterest.com"><img src="pinterest.png" width="37" height="37"/></a></td>
+                               </tr>
+                               <br>
+                            <tr>
+                                   <td  class="pic"><a href="www.youtube.com"><img src="youtube.png" width="37px" height="37px"/></a></td>
+                                   <td  class="pic"><a href="www.google+.com"><img src="google+.png" width="38px" height="38px"/></a></td>
+                                   <td  class="pic"><a href="www.email.com"><img src="email.png"width="38px" height="38px" /></a></td>
+                                   <td  class="pic"><a href="www.linkeldin.com"><img src="linkeldin.png" width="37" height="37"/></a></td>
+
+                               </tr>
+
+                           </table>
+                       </div>
+                   </div>
+                   <div class="col-md-12" id="rights">
+                       <p class="para">&copy Copyright 2020 All Rights Reserved. </p>
+                   </div>
+               </div>
+           </div>
+        </div>
+        <style>
+              .message{
+                  border: 1px solid;
+                  padding: 7px;
+                  margin-bottom: 15px;
+                  border-radius: 6px;
+              }
+            .head{
+                text-decoration: underline;
+            }
+
+            #footer{
+                border-radius: 5px;
+                border: 1px solid lightgray;
+                background-color: lightgrey;
+                height: 370px;
+                padding: 50px;
+
+            }
+            .nav{
+                color: white;;
+                font-family: Calibri;
+                font-size: 20px;
+            }
+            label{
+                color: black;
+            }
+            #navbar{
+                padding-left: 280px;
+
+            }
+              .motivation{
+                  padding: 10px;
+                  font-size: 16px;
+                  font-family:Calibri;
+                  text-align: left;
+                  text-display:block;
+
+              }
+              #account{
+                  padding-left: 535px;
+                  font-weight: bolder;
+
+              }
+              h1{
+                  font-family: "Arial Black";
+                  color: darkblue;
+                  padding-left: 0px;
+              }
+            .share{
+               float: right;
+                padding-right: 30px;
+            }
+            .follow{
+                padding-left: 30px;
+                font-family: Calibri;
+                font-size: 15px;
+            }
+            .para{
+                text-align: center;
+                font-family: sans-serif;
+                font-size: 15px;
+            }
+            #rights{
+                margin-top: 40px;
+            }
+            #subscribe{
+               margin-top: 10px;
+                width: 300px;
+                height: 50px;
+                padding-left: 0px;
+                font-family: "Arial Black";
+            }
+            #user{
+                padding-right: 15px;
+                font-weight:bolder;
+            }
+            .value{
+                padding-left: 10px;
+            }
+            dfd{
+                color:#ffe70a;
+            }
+            .pic{
+             padding: 10px;
+            }
+            .h3{
+                text-decoration: underline;
+            }
+            .image2_name{
+                padding-left:61px;
+                font-family:Calibri;
+                font-size: 20px;
+            }
+              .image1_name{
+
+                  font-family:Calibri;
+                  font-size: 18px;
+              }
+            #image_cat{
+                margin: 30px;
+                padding: 19px;
+
+            }
+            #image4{
+                float: right;
+            }
+
+        </style>
+
+    </body>
+</html>
